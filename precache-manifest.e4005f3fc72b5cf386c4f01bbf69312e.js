c3ca0fa1590bac230da828e952c842d96b7f3972
Amit Maurya <amit.maurya@ltts.com>
2020-06-08 21:56:03 +0530

    Updates

---
+++ precache-manifest.e4005f3fc72b5cf386c4f01bbf69312e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1a607d2b1dad8a00dea028022408817",
+    "url": "/infograin-test/index.html"
+  },
+  {
+    "revision": "91bde4c58c493552985f",
+    "url": "/infograin-test/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0d4ad8cda1231c9d8539",
+    "url": "/infograin-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "91bde4c58c493552985f",
+    "url": "/infograin-test/static/js/2.e4867ae4.chunk.js"
+  },
+  {
+    "revision": "f88b36470b8b650da4a18244e35dd001",
+    "url": "/infograin-test/static/js/2.e4867ae4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d4ad8cda1231c9d8539",
+    "url": "/infograin-test/static/js/main.ce96c4de.chunk.js"
+  },
+  {
+    "revision": "374f47b222ac360a96a7",
+    "url": "/infograin-test/static/js/runtime-main.01cac2cb.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/infograin-test/precache-manifest.e4005f3fc72b5cf386c4f01bbf69312e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/infograin-test/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
